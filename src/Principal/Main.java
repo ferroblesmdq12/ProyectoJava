7563d9c7bd6c77a665a64e4b9e8ae9fe4dadf172
ferroblesmdq12 <fernandomdq12@gmail.com>
2023-08-15 19:58:41 -0300

    Version inicial

---
+++ src/Principal/Main.java
@@ -0,0 +1,19 @@
+package Principal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/**
+		 * 
+		 * Desafío 1, Crear una rama con el nombre feature/desafio-java-1. 
+		 * El objetivo es trabajar la sintaxis de java, if/else y funciones, con validaciones sobre fechas 
+		 * 
+		 * 1. Ingresar una fecha por teclado: dd/mm/yyyy (dd int, mm int, yyyy int) 
+		 * validar que dd >=1 && <=31 TENIENDO EN CUENTA EL MES validar que mes >=1 && <=12 validar que año >=1900 && <= 2099
+			
+			Nota: Ojo, si el año es bisiesto, febrero tiene 29 días. Informar por consola si la fecha es correcta.
+		 */
+
+	}
+
+}
