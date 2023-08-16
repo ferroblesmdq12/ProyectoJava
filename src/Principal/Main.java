7b4490d17db15c2e0fdc97a60a65f579fec21a60
ferroblesmdq12 <fernandomdq12@gmail.com>
2023-08-16 17:34:00 -0300

    version 1.0 programa funcionando

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -1,8 +1,12 @@
 package Principal;
 
-public class Main {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class Main 
+{
+
+	public static void main(String[] args) 
+  {
 		/**
 		 * 
 		 * Desafío 1, Crear una rama con el nombre feature/desafio-java-1. 
@@ -14,8 +18,80 @@ public class Main {
 			Nota: Ojo, si el año es bisiesto, febrero tiene 29 días. Informar por consola si la fecha es correcta.
 		 */
 
-		System.out.println("Holaaa");
+		System.out.println("Ingrese datos de Fecha (dd/mm/aaaa): \n\n");
+		
+		Scanner ingresoTeclado = new Scanner(System.in);
+		
+		System.out.println("Ingrese dia: ");
+		int dia = ingresoTeclado.nextInt();
+		System.out.println("Ingrese mes: ");
+		int mes = ingresoTeclado.nextInt();
+		System.out.println("Ingrese Año: ");
+		int año = ingresoTeclado.nextInt();
+		
 
-	}
+		if(año >= 1900 && año <= 2099)
+		{
+			if(año % 4 == 0 && año !=0)
+			{
+				System.out.println(año + "Es año Biciesto");
+				
+				if(mes == 2) 
+				{
+					if(dia >= 1 && dia <= 29)
+					{
+						System.out.println("rango de dias ingresado correcto.");
+					}
+					else
+					{
+						System.out.println("Rango de dias incorrecto !");
+					}
+				}
+				else if(mes>=1 && mes<=12)
+				{
+					if(dia>=1 && dia<=31)
+					{
+						System.out.println("rango de dias ingresado correcto.");
+					}
+					else
+					{
+						System.out.println("Rango de dias incorrecto !");
+					}
+				}
+				else
+				{
+					System.out.println("Rango de mes incorrecto!");
+				}
+				
+					
+			    
+			}
+			else 
+			{
+				System.out.println("año cargado correctamente, No es Año Biciesto.");
+				
+			}
+		
+			
 
+		}
+		else
+		{
+			System.out.println("año ingresado fuera de Rango");
+		}
+			
+		
+		System.out.println("dia: " + dia + ",");
+		System.out.println("mes: " + mes + ",");
+		System.out.println("año: " + año );
+		
+		System.out.printf("Fecha: %d / %d / %d", dia, mes, año);
+		
+		
+		
+		
+		
+		
+
+  }
 }
